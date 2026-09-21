50251a3e2aed5e0dd8945eebfa3a9a30ac28ceaa
Thomas <thomaspippi.dev@gmail.com>
2026-09-21 08:42:09 -0300

    Interface

--- Aula08_Interfaces/Funcionarios/src/entidades/Vendedor.java
+++ Aula08_Interfaces/Funcionarios/src/entidades/Vendedor.java
@@ -1,5 +1,18 @@
 package entidades;
 
 public class Vendedor extends Funcionario {
+    private double metaVendas;
 
+    public Vendedor(String nome, double salario, double metaVendas) {
+        super(nome, salario);
+        this.metaVendas = metaVendas;
+    }
+
+    public double getMetaVendas() {
+        return metaVendas;
+    }
+
+    public void setMetaVendas(double metaVendas) {
+        this.metaVendas = metaVendas;
+    }
 }
